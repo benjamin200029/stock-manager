60e7feff90a65a6ac245f7f237a9d6fb6996e073
benjamin200029 <bade4943@Stu.raritanval.edu>
2019-10-09 12:53:52 -0400

    version 10/9/2019
    still need to fix addProduct method

--- StockManager.java
+++ StockManager.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
  * Manage the stock in a business.
  * The stock is described by zero or more Products.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Ben Adelson 
+ * @version (10/09/2019)
  */
 public class StockManager
 {
@@ -21,12 +21,16 @@ public class StockManager
     }
 
     /**
-     * Add a product to the list.
-     * @param item The item to be added.
+     * Print details of all the products which has stock
+     * levels below the given amount
      */
-    public void addProduct(Product item)
+        public void printLowStockProducts(int upperLimit)
     {
-        stock.add(item);
+        for(Product product : stock) {
+            if(product.getQuantity() < upperLimit) {
+                System.out.println(product.toString());
+            }
+        }
     }
     
     /**
@@ -37,7 +41,12 @@ public class StockManager
      */
     public void delivery(int id, int amount)
     {
+        Product product = findProduct(id);
+        if(product != null) {
+            product.increaseQuantity(amount);
+        } 
     }
+
     
     /**
      * Try to find a product in the stock with the given id.
@@ -45,11 +54,43 @@ public class StockManager
      *         with a matching ID.
      */
     public Product findProduct(int id)
+{
+        boolean found = false;
+        int indexOfProduct= 0;
+        while(indexOfProduct < stock.size() && !found){
+                if(stock.get(indexOfProduct).getID() == id){
+                    found = true;
+                }else{
+                    indexOfProduct++;
+                }
+        }
+        if(found){
+                return stock.get(indexOfProduct);
+        }else{
+                return null;
+        }
+}
+
+public Product findProduct(String name)
     {
-        return null;
+            boolean found = false;
+            int indexOfProduct = 0;
+            while(indexOfProduct < stock.size() && !found) {
+                   if(stock.get(indexOfProduct).getName().equals(name)) {
+                        found = true;
+                    }else {
+                        indexOfProduct++;
+                    }
+                }
+            if(found) {
+                    return stock.get(indexOfProduct);
+            }else {
+                   return null;
+            }
     }
-    
-    /**
+
+ 
+     /**
      * Locate a product with the given ID, and return how
      * many of this item are in stock. If the ID does not
      * match any product, return zero.
@@ -58,13 +99,43 @@ public class StockManager
      */
     public int numberInStock(int id)
     {
-        return 0;
+        Product product = findProduct(id);
+        if(product != null) {
+            return product.getQuantity();
+        }else {
+            return 0;
+        }
     }
+    
+    /**
+     * Add a product to the list, if there is not already one with a similar copy
+     * @param item The item to be added.
+     */
+    public void addProduct(Product item)
+    {
+       //if(findProduct(item.getID() == id)){
+            stock.add(item);
+      //  }
+
+    }
+
+    
+    // old version
+    
+    // public void addProduct(Product item)
+    // {
+        // stock.add(item);
+    // }
+    
+
 
     /**
      * Print details of all the products.
      */
     public void printProductDetails()
     {
+        for(Product product:stock){
+            System.out.println(product.toString());
+        }
     }
 }
